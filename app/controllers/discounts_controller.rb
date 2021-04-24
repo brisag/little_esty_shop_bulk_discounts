b86db13df473cd1be2a9959b406ab10452adab22
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-24 11:20:53 -0600

    Add controller params

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,11 +1,32 @@
 class DiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    discount = @merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to merchant_discounts_path(@merchant)
+    else
+      flash[:error] = "Missing Fields"
+      render :new
+    end
   end
 
   def show
     # binding.pry
     @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.find(params[:id])
+  end
+
+  private
+  def discount_params
+    params.permit(:percent_discount, :quantity_threshold)
   end
 end
