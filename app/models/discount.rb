c600b2dbe27fb6c91d24904899773daab48cd490
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-24 19:04:20 -0600

    Comment out WIP for bulk:

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,7 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+
+  validates_presence_of :percent_discount, :quantity_threshold
   validates :percent_discount, numericality: true
   validates :quantity_threshold, numericality: true
 end
