3d152dddfc8a3d289f7b1a0260a571c0bf54e055
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-28 07:59:09 -0600

    Add pedning invoices

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -7,18 +7,10 @@ class Discount < ApplicationRecord
   validates :percent_discount, numericality: true
   validates :quantity_threshold, numericality: true
 
-  # def pending_invoices
-  #   invoice_items.where(status: 1)
-  # end
 
-  # def pending_invoices
-  #   invoice_items
-  #   .where(status: 1)
-  #   .where('quantity >= ?', self.quantity_threshold)
-  # end
-  #
-  # def pending_invoices_view_logic
-  #   return "edit_delete" if pending_invoices.empty?
-  #   "message"
-  # end
+  def pending_invoices
+    invoice_items
+    .where(status: 1)
+    .where('quantity >= ?', self.quantity_threshold)
+  end
 end
